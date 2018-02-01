50ad5284ace1e49371e748f2bb5c6e824e50da1f
KingScooty <scott.vernon@gmail.com>
2018-02-01 20:28:25 +0000

    Get data passing to a widget!

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Description
+
+```bash
+docker run \
+    -v=/Users/kingscooty/downloads/dashing/dashboards:/dashboards \
+    -v=/Users/kingscooty/downloads/dashing/widgets:/widgets \
+    -v=/Users/kingscooty/downloads/dashing/jobs:/jobs \
+    -d -p 8080:3030 kingscooty/smashing
+```
--- jobs/health.rb
+++ jobs/health.rb
@@ -1,14 +1,32 @@
 require 'net/http'
+require 'net/https'
 require 'json'
+require 'uri'
 
 SCHEDULER.every '60s', first_in: 0 do |job|
-  http = Net::HTTP.new('nginx-proxy1.dev.web.eu-west-1.traveljigsaw.net')
-  response = http.request(Net::HTTP::Get.new("/health/aggregator"))
+
+  puts '*** Running job ***'
+
+  # http = Net::HTTP.new('nginx-proxy1.dev.web.eu-west-1.traveljigsaw.net')
+  # response = http.request(Net::HTTP::Get.new("/health/aggregator"))
+
+  uri = URI.parse('https://status.github.com/api/status.json')
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+
   json = JSON.parse(response.body)
 
+  puts '*** JSON RESPONSE: ***'
+  puts json
+  puts '*** ****'
+
   if json.count <= 0
-    send_event('Health', statusText: "failed")
+    send_event('health', statustext: "failed")
   else
-    send_event('Health', statusText: "#{json['status']}")
+    send_event('health', statustext: "#{json['status']}")
   end
 end
