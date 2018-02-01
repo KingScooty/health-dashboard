b1b585c82709cc9d2da28f94d3a3cda6b87ec716
stocktont <thomasstockton@rentalcars.com>
2018-02-01 16:53:17 +0000

    Update Ruby Script

--- jobs/health.rb
+++ jobs/health.rb
@@ -2,29 +2,13 @@ require 'net/http'
 require 'json'
 
 SCHEDULER.every '60s', first_in: 0 do |job|
-  #http = Net::HTTP.new('status.github.com/api/status.json')
-
-  uri = URI('https://status.github.com/api/status.json')
-
-  Net::HTTP.start(uri.host, uri.port,
-    :use_ssl => uri.scheme == 'https') do |http|
-    request = Net::HTTP::Get.new uri
-
-    response = http.request request # Net::HTTPResponse object
-  end
-
-  # response = http.request(Net::HTTP::Get.new("/api/status.json"))
-  json = JSON.parse(response)
+  http = Net::HTTP.new('nginx-proxy1.dev.web.eu-west-1.traveljigsaw.net')
+  response = http.request(Net::HTTP::Get.new("/health/aggregator"))
+  json = JSON.parse(response.body)
 
   if json.count <= 0
-    send_event('status', statusText: "failed")
+    send_event('Health', statusText: "failed")
   else
-    send_event('status', statusText: "#{json.status}")
+    send_event('Health', statusText: "#{json['status']}")
   end
 end
-
-# docker run \
-#     -v=/Users/kingscooty/downloads/dashing/dashboards:/dashboards \
-#     -v=/Users/kingscooty/downloads/dashing/widgets:/widgets \
-#     -v=/Users/kingscooty/downloads/dashing/jobs:/jobs \
-#     -d -p 8080:3030 kingscooty/smashing
